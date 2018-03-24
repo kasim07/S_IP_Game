4d2cb765ac3742cf1337c88f0b5590e7f6c1ce29
Kasim-PC\Kasim <kasim3112@naver.com>
2018-03-24 17:52:49 +0900

    AdMob 초기 설정 초기 작업준비 및 더미 이미지 생성

---
+++ Assets/02.Scripts/Ads/AdMobManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using admob;
+
+public class AdMobManager : MonoBehaviour {
+    public static AdMobManager Instance;
+    private string android_banner_id = "ca-app-pub-8512629740125748/6803655315";
+    private string android_interstitial_id = "ca-app-pub-8512629740125748/5965403719";
+
+    public bool isShow;
+
+
+    void Start()
+    {
+        Instance = this;
+        isShow = true;
+        initAdmob();
+    }
+
+    void initAdmob()
+    {
+        Admob ad = Admob.Instance();
+        ad.bannerEventHandler += onBannerEvent;
+        ad.interstitialEventHandler += onInterstitialEvent;
+        ad.rewardedVideoEventHandler += onRewardedVideoEvent;
+        ad.nativeBannerEventHandler += onNativeBannerEvent;
+        ad.initAdmob(android_banner_id, android_interstitial_id);
+    }
+
+    void onInterstitialEvent(string eventName, string msg)
+    {
+    }
+    void onBannerEvent(string eventName, string msg)
+    {
+    }
+    void onRewardedVideoEvent(string eventName, string msg)
+    {
+    }
+    void onNativeBannerEvent(string eventName, string msg)
+    {
+    }
+    private void fixTransform(Vector3 size, Vector3 pos)
+    {
+    }
+
+    public void ShowBannerAd()
+    {
+        isShow = true;
+        Admob.Instance().showBannerRelative(AdSize.Banner, AdPosition.TOP_CENTER, 0);
+    }
+
+    public void CloseBannerAd()
+    {
+        isShow = false;
+        Admob.Instance().removeBanner();
+    }
+}
